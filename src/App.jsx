6264c7a16083e4d9a39915e415ffca4a32e61e8b
DoctorAivan <design.villarroel.s@gmail.com>
2024-06-11 00:19:17 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './routes/Home'
+
+function App() {
+
+  return (
+    <Routes>
+      <Route index element={<Home />} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ src/routes/Home.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom"
+import LogoWeb from "../images/gala-cracks-web.jpg"
+import LogoMobile from "../images/gala-cracks-mobile.jpg"
+
+
+const Home = () => {
+  return (
+    <div className="h-screen grid place-items-center
+    bg-gradient-to-b from-slate-900 to-slate-800">
+
+      <a className="hover:animate-buttonActive" href="https://www.youtube.com/live/CKHBWXffPlg">
+
+        <div className="w-auto mx-6 rounded-3xl overflow-hidden
+        shadow-2xl shadow-slate-900/80">
+          <img className="hidden md:block" src={LogoWeb} />
+          <img className="block md:hidden" src={LogoMobile} />
+        </div>
+
+      </a>
+    </div>
+  )
+}
+
+export default Home
